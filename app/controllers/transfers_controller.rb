273354676bbe6a2c00e11c8d74aaf907a6ce0c8d
Daniel Dvorkin <ddvorkin@joinhandshake.com>
2025-04-07 20:04:42 -0400

    small optimization

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -2,6 +2,9 @@ class TransfersController < ApplicationController
   def index
     FetchTransactionsJob.perform_later
     
+    # 1. joins is for filtering: It ensures that the SQL query includes the actions table so that the where condition can be applied.
+    # 2. includes is for performance: It preloads the actions association to reduce the number of database queries when accessing actions later.
+    # 3. Combining them: Using both joins and includes allows you to filter records based on the actions table while also optimizing performance when accessing the actions data.
     @transfers = if params[:action_type].present?
              Transaction.joins(:actions)
                   .where(actions: { action_type: params[:action_type] })
@@ -10,8 +13,7 @@ class TransfersController < ApplicationController
                   .distinct
                   .paginate(page: params[:page], per_page: 10)
            else
-             Transaction.joins(:actions)
-                  .includes(actions: [])
+             Transaction.includes(actions: [])
                   .order(created_at: :desc)
                   .distinct
                   .paginate(page: params[:page], per_page: 10)
